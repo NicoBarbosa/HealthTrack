b4d03f3e3e6a62407d82a87425c9dee2b5ffde07
Rodrigo Zandonadi <rodzandonadi@gmail.com>
2021-11-20 20:54:25 -0300

    Add getters and setters to Usuario.java

--- src/healthtrack/bean/Usuario.java
+++ src/healthtrack/bean/Usuario.java
@@ -12,7 +12,9 @@ public class Usuario {
     private double altura;
     private Calendar dtInclusao;
 
-    public Usuario() { super(); }
+    public Usuario() { 
+    	super(); 
+	}
 
     public Usuario(
         int cd_usuario,
@@ -22,17 +24,81 @@ public class Usuario {
         Calendar dtNascimento,
         char sexo,
         double altura,
-        Calendar dtInclusao,
+        Calendar dtInclusao
     ) {
         this.cd_usuario = cd_usuario;
         this.nome = nome;
         this.email = email;
-        this.senha, = senha;
+        this.senha = senha;
         this.dtNascimento = dtNascimento;
         this.sexo = sexo;
         this.altura = altura;
         this.dtInclusao = dtInclusao;
     }
+
+	public int getCd_usuario() {
+		return cd_usuario;
+	}
+
+	public void setCd_usuario(int cd_usuario) {
+		this.cd_usuario = cd_usuario;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public Calendar getDtNascimento() {
+		return dtNascimento;
+	}
+
+	public void setDtNascimento(Calendar dtNascimento) {
+		this.dtNascimento = dtNascimento;
+	}
+
+	public char getSexo() {
+		return sexo;
+	}
+
+	public void setSexo(char sexo) {
+		this.sexo = sexo;
+	}
+
+	public double getAltura() {
+		return altura;
+	}
+
+	public void setAltura(double altura) {
+		this.altura = altura;
+	}
+
+	public Calendar getDtInclusao() {
+		return dtInclusao;
+	}
+
+	public void setDtInclusao(Calendar dtInclusao) {
+		this.dtInclusao = dtInclusao;
+	}
 
     
 }
