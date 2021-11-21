da97029b75c4420441e0b86ef819d6a8dc0f5914
Rodrigo Zandonadi <rodzandonadi@gmail.com>
2021-11-20 22:38:48 -0300

    Add UsuarioDAO Method to DAOFactory

--- src/healthtrack/factory/DAOFactory.java
+++ src/healthtrack/factory/DAOFactory.java
@@ -1,6 +1,11 @@
 package healthtrack.factory;
 
-public class DAOFactory {
+import healthtrack.dao.UsuarioDAO;
+import healthtrack.implement.OracleUsuarioDAO;
 
+public class DAOFactory {
 	
+	public static UsuarioDAO getUsuarioDAo() {
+		return new OracleUsuarioDAO();
+	}
 }
