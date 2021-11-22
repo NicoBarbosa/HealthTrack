74c8b88abd9562ec3a103a21d799b3f9ecd61239
Mizack <61763834+Mizack@users.noreply.github.com>
2021-11-21 22:52:48 -0300

    Alimento atualizado

--- src/healthtrack/factory/DAOFactory.java
+++ src/healthtrack/factory/DAOFactory.java
@@ -1,11 +1,18 @@
 package healthtrack.factory;
 
+import healthtrack.dao.AlimentoDAO;
 import healthtrack.dao.AtvFisicaDAO;
 import healthtrack.dao.UsuarioDAO;
+import healthtrack.implement.OracleAlimentoDAO;
 import healthtrack.implement.OracleAtvFisicaDAO;
 import healthtrack.implement.OracleUsuarioDAO;
 
 public class DAOFactory {
+	//Mizack
+	public static AlimentoDAO getAlimentoDAO() {
+		return new OracleAlimentoDAO();
+	}
+	
 	//Rodrigo
 	public static UsuarioDAO getUsuarioDAO() {
 		return new OracleUsuarioDAO();
