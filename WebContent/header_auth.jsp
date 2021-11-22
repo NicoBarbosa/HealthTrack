5c772128f5c27763782dfd738fd54565560998fa
Rodrigo Zandonadi <rodzandonadi@gmail.com>
2021-11-21 22:15:27 -0300

    Refactory cadastrar usuario jsp

--- WebContent/header_auth.jsp
+++ WebContent/header_auth.jsp
@@ -1,6 +1,4 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1" pageEncoding="ISO-8859-1"%>
-<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
-<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>
 <!DOCTYPE html>
 <html>
 
@@ -9,7 +7,6 @@
   <title>Health Track</title>
   <link rel="stylesheet" type="text/css" href="css/signin.css">
   <link rel="stylesheet" type="text/css" href="resources/css/bootstrap.min.css">
-  <link rel="stylesheet" href="css/style.css">
   <style>
     .bd-placeholder-img {
       font-size: 1.125rem;
---
+++ src/healthtrack/controller/UsuarioServlet.java
@@ -0,0 +1,68 @@
+package healthtrack.controller;
+
+import java.io.IOException;
+import java.util.Calendar;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import healthtrack.bean.Usuario;
+import healthtrack.dao.UsuarioDAO;
+import healthtrack.exception.DBException;
+import healthtrack.factory.DAOFactory;
+
+/**
+ * Servlet implementation class UsuarioServlet
+ */
+@WebServlet("/UsuarioServlet")
+public class UsuarioServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    
+    private UsuarioDAO dao;
+    
+    @Override
+    public void init() throws ServletException {
+    	super.init();
+    	dao = DAOFactory.getUsuarioDAO();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		try {
+			String nome = request.getParameter("nome");
+			String email = request.getParameter("email");
+			String senha = request.getParameter("senha");
+			
+			Usuario usuario = new Usuario();
+			
+			usuario.setNome(nome);
+			usuario.setEmail(email);
+			usuario.setSenha(senha);
+			
+			dao.cadastrar(usuario);
+			request.setAttribute("msg", "Usuário cadastrado");
+			
+		} catch(DBException db) {
+			db.printStackTrace();
+			request.setAttribute("erro", "Erro ao cadastrar o usuário");
+		} catch(Exception e) {
+			e.printStackTrace();
+			request.setAttribute("erro",  "Por favor, valide os dados");
+		}
+		request.getRequestDispatcher("cadastro.jsp").forward(request, response);
+	}
+
+}
