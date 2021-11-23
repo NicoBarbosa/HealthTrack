3f714899f57d732738e13661c4b138273e94b097
Mizack <61763834+Mizack@users.noreply.github.com>
2021-11-22 23:33:04 -0300

    Peso servlet

---
+++ src/healthtrack/controller/PesoServlet.java
@@ -0,0 +1,126 @@
+package healthtrack.controller;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import healthtrack.bean.Peso;
+import healthtrack.dao.PesoDAO;
+import healthtrack.exception.DBException;
+import healthtrack.factory.DAOFactory;
+
+/**
+ * Servlet implementation class UsuarioServlet
+ */
+@WebServlet("/peso")
+public class PesoServlet {
+	private PesoDAO dao;
+	@Override
+    public void init() throws ServletException {
+    	super.init();
+    	dao = DAOFactory.getPesoDAO();
+    }
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+    @Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String acao = request.getParameter("acao");
+		
+		switch (acao) {
+		case "listar":
+			listar(request, response);
+			break;
+		case "abrir-form-edicao":
+			int id = Integer.parseInt(request.getParameter("codigo"));
+			Peso peso = dao.buscar(id);
+			request.setAttribute("peso", peso);
+			request.getRequestDispatcher("peso-edicao.jsp").forward(request, response);
+		}	
+	}
+    private void listar(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		List<Peso> lista = dao.listar();
+		request.setAttribute("peso", lista);
+		request.getRequestDispatcher("peso.jsp").forward(request, response);
+	}
+    /**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String acao = request.getParameter("acao");
+		
+		switch (acao) {
+		case "cadastrar":
+			cadastrar(request, response);
+			break;
+		case "editar":
+			editar(request,response);
+		case "excluir":
+			excluir(request,response);
+			break;
+		}
+	}
+	
+	private void cadastrar(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		try {
+			double vlPeso = Double.parseDouble(request.getParameter("vl-peso"));
+			
+			Peso peso = new Peso(0,0,vlPeso, Calendar.getInstance());
+			
+			dao.cadastrar(peso);
+			
+			request.setAttribute("msg", "Peso cadastrado");
+			
+		} catch(DBException db) {
+			db.printStackTrace();
+			request.setAttribute("erro", "Erro ao cadastrar alimento");
+		} catch(Exception e) {
+			e.printStackTrace();
+			request.setAttribute("erro",  "Por favor, valide os dados");
+		}
+		request.getRequestDispatcher("alimentos.jsp").forward(request, response);
+	}
+	
+	private void editar(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		try {
+			int codigo = Integer.parseInt(request.getParameter("codigo"));
+			double vlPeso = Double.parseDouble(request.getParameter("vl-peso"));
+			SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+			Calendar data = Calendar.getInstance();
+			data.setTime(format.parse(request.getParameter("data-ali")));
+			
+			Peso peso = new Peso(codigo,vlPeso,data);
+			
+			dao.atualizar(peso);
+
+			request.setAttribute("msg", "Alimento atualizado!");
+		} catch (DBException db) {
+			db.printStackTrace();
+			request.setAttribute("erro", "Erro ao atualizar");
+		} catch (Exception e) {
+			e.printStackTrace();
+			request.setAttribute("erro", "Por favor, valide os dados");
+		}
+		listar(request,response);
+	}
+	private void excluir(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		int codigo = Integer.parseInt(request.getParameter("codigo"));
+		try {
+			dao.remover(codigo);
+			request.setAttribute("msg", "Alimento removido!");
+		} catch (DBException e) {
+			e.printStackTrace();
+			request.setAttribute("erro", "Erro ao atualizar");
+		}
+		listar(request,response);
+	}
+
+}
